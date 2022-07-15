82cd380a6e9ed2eaf522a29d206674a83f61b422
Debian <bitnami@ip-172-26-7-78.ca-central-1.compute.internal>
2022-07-15 11:53:22 +0000

    first commit

---
+++ backend/src/api/termes-et-conditions/services/termes-et-conditions.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * termes-et-conditions service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::termes-et-conditions.termes-et-conditions');
