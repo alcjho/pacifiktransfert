bbb8bb69e4cbec21c72f03d37eb8bda7c3f7a822
tech-renoquotes-old <jalcius@renoquotes.com>
2022-07-26 02:33:54 -0400

    add console.log to login api

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -30,7 +30,6 @@ function Login() {
 
         try {
             await axios.post('/api/login', userData);
-            console.log(userData)
             const redirectUrl = router.query['redirect']
             router.replace(redirectUrl?redirectUrl:'/transactions');
         } catch (err) {
