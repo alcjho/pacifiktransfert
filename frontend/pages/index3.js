7030a7837a36d698b10de03018eb1e53cc83df63
tech-renoquotes-old <jalcius@renoquotes.com>
2022-07-26 19:08:46 -0400

    switch index names

--- frontend/pages/index3.js
+++ frontend/pages/index3.js
@@ -1,55 +1,56 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import Navbar from '../components/Layouts/Navbar';
-import MainBanner from '../components/HomeThree/MainBanner';
-import FeaturedCard from '../components/HomeThree/FeaturedCard';
-import HowItWorks from '../components/HomeThree/HowItWorks';
-import ServicesContent from '../components/HomeThree/ServicesContent';
-import ComparisonsTableTwo from '../components/Common/ComparisonsTableTwo';
-import OurFeaturesStyleTwo from '../components/Common/OurFeaturesStyleTwo';
+import MainBanner from '../components/HomeOne/MainBanner';
+import FeaturedCard from '../components/HomeOne/FeaturedCard';
+import ServicesCard from '../components/Common/ServicesCard';
+import ComparisonsTable from '../components/Common/ComparisonsTable';
+import OurFeaturesContent from '../components/Common/OurFeaturesContent';
 import EasyPaymentBorrow from '../components/Common/EasyPaymentBorrow';
-import FunFacts from '../components/HomeThree/FunFacts';
+import FunFacts from '../components/HomeOne/FunFacts';
 import CustomersFeedback from '../components/Common/CustomersFeedback';
 import PartnerContent from '../components/Common/PartnerContent';
-import AppDownloadContent from '../components/HomeThree/AppDownloadContent';
+import AppDownloadContent from '../components/HomeOne/AppDownloadContent';
 import AccountCreateArea from '../components/Common/AccountCreateArea';
 import BlogCard from '../components/Common/BlogCard';
 import Footer from '../components/Layouts/Footer';
 import Rates from '../components/Rates/Rates';
-import axios from 'axios';
-import { BACKEND_URL } from '../config/constant';
 
-export default function Home() {
-    const [homeInfo, setHomeInfo] = useState({})
-    const getHome = async () => {
-        axios.get(BACKEND_URL+'/api/home-page', {params: {populate:'*'}})
-          .then(function (response) {
-            console.log('img', response.data.data.attributes.step1_icon?.data.attributes.url);
-            setHomeInfo(response.data.data.attributes)
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-    }
-      
-      useEffect(() => {
-        getHome()
-      }, [])
-      
-
-
+class Index extends Component {
+    render() {
         return (
             <>
                 <Navbar />
+                
+                <MainBanner />
 
-                <MainBanner homeInfo={homeInfo} />
+                <FeaturedCard />
 
-                <FeaturedCard homeInfo={homeInfo} />
+                <Rates />
 
+                <ServicesCard />
 
-                <HowItWorks homeInfo={homeInfo} />
+                <ComparisonsTable />
 
+                <OurFeaturesContent />
 
-                <Footer homeInfo={homeInfo} />
+                <EasyPaymentBorrow />
+
+                <FunFacts />
+
+                <CustomersFeedback />
+
+                <PartnerContent />
+
+                <AppDownloadContent />
+
+                <AccountCreateArea />
+
+                <BlogCard />
+
+                <Footer />
             </>
         );
     }
+}
+
+export default Index;
